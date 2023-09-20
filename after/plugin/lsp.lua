9f1019eb67c776e1f6895fa893e38042835948cc
Wreigh <wreighchristian.santos@infor.com>
2023-09-20 21:26:39 +0800

    update

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -16,5 +16,14 @@ lsp.setup_nvim_cmp({
   mapping = cmp_mappings
 })
 
+lsp.on_attach(function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+end)
+
 lsp.setup()
 
